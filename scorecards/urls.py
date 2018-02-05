aad4dcfd58ab28467e2836ab3eb4aaf0a4e5f7d5
Olle Andersson <oll@kth.se>
2018-02-05 20:07:22 +0100

    Added new scorecard creation route, select_course

--- scorecards/urls.py
+++ scorecards/urls.py
@@ -14,5 +14,8 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/select_round/$', views.select_round, name='select_round'),
 
     # scorecards/fill_scorecard/$
-    url(r'^fill_scorecard/$', views.fill_scorecard, name='fill_scorecard')
+    url(r'^fill_scorecard/$', views.fill_scorecard, name='fill_scorecard'),
+
+    # scorecards/select_course/$
+    url(r'^select_course/$', views.select_course, name='select_course')
     ]
