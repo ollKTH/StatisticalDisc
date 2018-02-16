fcd9987c2aa41fbe750cf08081a1d4f153781d91
Olle Andersson <oll@kth.se>
2018-02-16 22:21:20 +0100

    Fixed broken urls

--- courses/urls.py
+++ courses/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     url(r'^courseoverview/$', views.courseoverview, name='courseoverview'),
 
     # courses/javascript_test_page/
-    url(r'javascript_test_page', views.javascript_test_page, name='javascript_test_page')
+    url(r'^javascript_test_page/$', views.javascript_test_page, name='javascript-test-page'),
 
     ]
--- scorecards/urls.py
+++ scorecards/urls.py
@@ -2,20 +2,22 @@ from django.conf.urls import url
 
 from . import views
 
+app_name='scorecards'
+
 urlpatterns = [
     
     # scorecards/
     url(r'^$', views.scorecards, name='scorecards'),
 
-    # scorecards/scorecarddetails/(?P<pk>\d+)/$
-    url(r'^scorecarddetails/(?P<pk>\d+)/$', views.scorecarddetails, name='scorecarddetails'),
-
-    # scorecards/select_round/$
-    url(r'^(?P<pk>\d+)/select_round/$', views.select_round, name='select_round'),
-
     # scorecards/fill_scorecard/$
     url(r'^fill_scorecard/$', views.fill_scorecard, name='fill_scorecard'),
 
+    # scorecards/scorecarddetails/5/$
+    url(r'^scorecarddetails/(?P<pk>\d+)/$', views.scorecarddetails, name='scorecarddetails'),
+
+    # scorecards/5/select_round/$
+    url(r'^(?P<pk>\d+)/select_round/$', views.select_round, name='select_round'),
+
     # scorecards/select_course/$
-    url(r'^select_course/$', views.select_course, name='select_course')
+    url(r'^select_course/$', views.select_course, name='select_course'),
     ]
