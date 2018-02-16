6fc74d72d66b26335bf7f971e95e10f60daaa596
Olle Andersson <oll@kth.se>
2018-02-16 22:22:00 +0100

    Added ScoreTableMini

--- scorecards/tables.py
+++ scorecards/tables.py
@@ -6,15 +6,27 @@ from .models import Scorecard, Score
 # TODO : Implement some styling highlightning the selected row from these tables
 
 class ScoreCardTable(tables.Table):
-    course = tables.LinkColumn('scorecarddetails', args=[A('pk')])
+    course = tables.LinkColumn('scorecards:scorecarddetails', args=[A('pk')])
 
     class Meta:
         model = Scorecard
         template = 'django_tables2/bootstrap.html'
         exclude = ('id')
 
+# Don't really know if used
 class ScoreTable(tables.Table):
     class Meta:
         model = Score
         template = 'django_tables2/bootstrap.html'
         exclude = ('id', 'scorecard')
+
+# Mini version of the scorecard table
+class ScoreCardTableMini(tables.Table):
+    round = tables.LinkColumn('scorecards:scorecarddetails', args=[A('pk')])
+
+
+    class Meta:
+        model = Scorecard
+        template = 'django_tables2/bootstrap.html'
+        exclude = ('id', 'date_played', 'noOfHoles', 'user')
+        orderable = False
