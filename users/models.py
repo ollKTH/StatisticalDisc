533ccea818a910faf5eb117adda6596fe48f8fd9
Olle Andersson <oll@kth.se>
2018-02-16 22:19:56 +0100

    Updated

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+# One-to-One link extending the user class for adding additional data and functions
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def get_no_of_rounds(self):
+        scorecards = self.user.scorecard_set.all()
+        i = 0
+        for scorecard in scorecards:
+            i += 1
+        return i
+
+# Create profile object that is related to a created user object
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+# Save profile whenever the related user is saved
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
