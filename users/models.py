bbfcc7c82debb318350157d5cac8d0bbad5d0fd9
Olle Andersson <oll@kth.se>
2018-02-26 19:01:03 +0100

    Added lots of statistical methods for user

--- users/models.py
+++ users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+import operator
+
+from scorecards.models import Scorecard, Score
 
 # One-to-One link extending the user class for adding additional data and functions
 class Profile(models.Model):
@@ -12,10 +15,81 @@ class Profile(models.Model):
 
     def get_no_of_rounds(self):
         scorecards = self.user.scorecard_set.all()
-        i = 0
+        return len(scorecards)
+
+    # Get average score on a specific round
+    def get_round_average(self, course, round):
+        scorecards = get_scorecards_by_round(self, round, course)
+
+        for scorecard  in scorecards:
+            total_score += scorecard.total_score
+
+        average = total_score / len(scorecards)
+        return average
+
+    # Get the average score on a single hole
+    def get_hole_average(self, course, hole):
+        scorecards = Scorecard.objects.filter(course = course, user = self.user)
+
+        scores = []
+        total_score = 0
+
         for scorecard in scorecards:
-            i += 1
-        return i
+            try:
+                scores.append(scorecard.score_set.get(hole = hole)) # Unsure if this will work
+            except Score.DoesNotExist:
+                # No score at this round
+                go = None
+
+        for score in scores:
+            total_score += score.score
+
+        # Avoid div by 0
+        if total_score > 0:
+            average = total_score / len(scores)
+        else:
+            average = 0
+        return average
+
+# QUERY FUNCTIONS #
+
+    # Get scorecards by given course
+    def get_scorecards_by_course(self, course, order = None):
+        if order is None:
+            scorecards = Scorecard.objects.filter(course = course, user = self.user)
+        else:
+            scorecards = Scorecard.objects.filter(course = course, user = self.user).order_by(order)
+        return scorecards
+
+    # Get scorecards by given round
+    def get_scorecards_by_round(self, round, course, order = None):
+        if order is None:
+            scorecards = Scorecard.objects.get(round = round, course = course, user = self.user)
+        else:
+            scorecards = Scorecard.objects.filter(round = round, course = course, user = self.user).order_by(order)
+        return scorecards
+    
+    # Get the most played course for a user
+    def get_most_played_course(self):
+        scorecards = Scorecard.objects.filter(user = self.user)
+
+        statistics = dict()
+        
+        # Generate a dict with all played courses, extracting this info from the scorecards
+        for scorecard in scorecards:
+            course = scorecard.course
+            # If course has entry in dict, add one to that index
+            if course.course_name in statistics:
+                statistics[course.course_name] += 1
+            # Else, create that dict position and set to 1
+            else:
+                statistics[course.course_name] = 1
+
+        max_course = max(statistics.items(), key=operator.itemgetter(1))[0]
+        times = statistics[max_course]
+        return max_course, times
+
+# USER LINKAGE FUNCTIONS #
 
 # Create profile object that is related to a created user object
 @receiver(post_save, sender=User)
